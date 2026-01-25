5c96d2315240ca25e8b7523ab8e5dd4b8ddbbee0
hasib_inamdar <hasib.inamdar.pro@gmail.com>
2026-01-26 03:32:56 +0530

    Added store provider to the App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App'
+import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
