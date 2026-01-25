cc5cffb4d62602b3def67665f15b25bc77477fea
hasib_inamdar <hasib.inamdar.pro@gmail.com>
2026-01-26 03:36:32 +0530

    Resolved store import problem

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App'
 import { Provider } from 'react-redux'
+import { store } from './store'
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "./features/auth/authApi";
 import { authSlice } from "./features/auth/authSlice";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     [authApi.reducerPath]: authApi.reducer,
