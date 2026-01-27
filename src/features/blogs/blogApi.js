7e3f0439714bd68d2cd03594d0a0ba5bf1b2161e
hasib_inamdar <hasib.inamdar.pro@gmail.com>
2026-01-27 14:56:09 +0530

    Added Blog API endpoints

--- src/features/blogs/blogApi.js
+++ src/features/blogs/blogApi.js
@@ -2,14 +2,30 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const blogApi = createApi({
   reducerPath: "blogs",
+
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3001",
   }),
+
   tagTypes: ["blogs"],
+
   endpoints: (builder) => ({
     getAllBlogs: builder.query({
       query: () => "/blogs",
       providesTags: ["blogs"],
+    }),
+
+    getCurrentUserBlogs: builder.query({
+      query: (userId) => `/blogs?authorId=${userId}`,
+      providesTags: ["blogs"],
+    }),
+
+    addBlog: builder.mutation({
+      query: (blog) => ({
+        url: "/blogs",
+        method: "POST",
+        body: blog,
+      }),
     }),
   }),
 });
